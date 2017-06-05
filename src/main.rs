136283aee652b6c9f9facf77dc039cdcf227f47a
Pedro Côrte-Real <pedro@pedrocr.net>
2017-06-05 17:17:28 +0100

    Make the transformation RGBE->XYZ
    Use four channels of input as that's what you actually have to do
    in camera processing.

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,19 @@ fn incr(val: f32, incr: f32) -> f32 {
 fn main() {
   let num_pixels = 100000000; // 100MP image
   // CAM to RGB matrix taken from the Sony A77V
-  let matrix: [[f32;3];3] = [
-    [0.75925,      0.21533987,  0.020964727], 
-    [0.28515315,   1.0129657,  -0.2981189], 
-    [0.021620918, -0.19883351,  1.177143],
+  let matrix: [[f32;4];3] = [
+    [0.75925,      0.21533987,  0.020964727, 0.0],
+    [0.28515315,   1.0129657,  -0.2981189,   0.0],
+    [0.021620918, -0.19883351,  1.177143,    0.0],
   ];
 
-  let mut inb = vec![0f32; num_pixels*3];
-  let mut out = vec![0f32; num_pixels*3];
+  let mut inb = vec![0f32; num_pixels*4];
 
   // Fill in the input buffer with different rgb values
   let mut r = 0f32;
   let mut g = 0f32;
   let mut b = 0f32;
-  for pix in inb.chunks_mut(3) {
+  for pix in inb.chunks_mut(4) {
     r = incr(r, 0.01);
     g = incr(g, 0.02);
     b = incr(b, 0.03);
@@ -30,17 +29,20 @@ fn main() {
     pix[0] = r;
     pix[1] = g;
     pix[2] = b;
+    pix[3] = 0f32;
   }
 
+  let mut out = vec![0f32; num_pixels*3];
   let from_time = time::precise_time_ns();
-  for (pixin, pixout) in inb.chunks(3).zip(out.chunks_mut(3)) {
+  for (pixin, pixout) in inb.chunks(4).zip(out.chunks_mut(3)) {
     let r = pixin[0];
     let g = pixin[1];
     let b = pixin[2];
+    let e = pixin[3];
 
-    pixout[0] = r * matrix[0][0] + g * matrix[0][1] + b * matrix[0][2];
-    pixout[1] = r * matrix[1][0] + g * matrix[1][1] + b * matrix[1][2];
-    pixout[2] = r * matrix[2][0] + g * matrix[2][1] + b * matrix[2][2];
+    pixout[0] = r * matrix[0][0] + g * matrix[0][1] + b * matrix[0][2] + e * matrix[0][3];
+    pixout[1] = r * matrix[1][0] + g * matrix[1][1] + b * matrix[1][2] + e * matrix[1][3];
+    pixout[2] = r * matrix[2][0] + g * matrix[2][1] + b * matrix[2][2] + e * matrix[2][3];
   }
   let to_time = time::precise_time_ns();
 
--- test.c
+++ test.c
@@ -6,10 +6,10 @@
 #define NUM_PIXELS (1000*100000) // 100 MP image
 
 // CAM to RGB matrix taken from the Sony A77V
-float matrix[3][3] = {
-  {0.75925,      0.21533987,  0.020964727}, 
-  {0.28515315,   1.0129657,  -0.2981189}, 
-  {0.021620918, -0.19883351,  1.177143},
+float matrix[3][4] = {
+  {0.75925,      0.21533987,  0.020964727, 0},
+  {0.28515315,   1.0129657,  -0.2981189,   0},
+  {0.021620918, -0.19883351,  1.177143,    0},
 };
 
 int64_t time_in_micros () {
@@ -29,7 +29,7 @@ float incr(float val, float incr) {
 }
 
 int main(void) {
-  float *in = (float *) malloc(NUM_PIXELS*3*sizeof(float));
+  float *in = (float *) malloc(NUM_PIXELS*4*sizeof(float));
   float *out = (float *) malloc(NUM_PIXELS*3*sizeof(float));
 
   // Fill in the input buffer with different rgb values
@@ -41,16 +41,17 @@ int main(void) {
     g = incr(g, 0.02);
     b = incr(b, 0.03);
 
-    in[i*3+0] = r;
-    in[i*3+1] = g;
-    in[i*3+2] = b;
+    in[i*4+0] = r;
+    in[i*4+1] = g;
+    in[i*4+2] = b;
+    in[i*4+3] = 0;
   }
 
   int64_t from_time = time_in_micros();
   for(int i=0; i<NUM_PIXELS; i++) {
-    out[i*3+0] = in[i*3+0] * matrix[0][0] + in[i*3+1] * matrix[0][1] + in[i*3+2] * matrix[0][2];
-    out[i*3+1] = in[i*3+0] * matrix[1][0] + in[i*3+1] * matrix[1][1] + in[i*3+2] * matrix[1][2];
-    out[i*3+2] = in[i*3+0] * matrix[2][0] + in[i*3+1] * matrix[2][1] + in[i*3+2] * matrix[2][2];
+    out[i*3+0] = in[i*4+0] * matrix[0][0] + in[i*4+1] * matrix[0][1] + in[i*4+2] * matrix[0][2] + in[i*4+3] * matrix[0][3];
+    out[i*3+1] = in[i*4+0] * matrix[1][0] + in[i*4+1] * matrix[1][1] + in[i*4+2] * matrix[1][2] + in[i*4+3] * matrix[1][3],
+    out[i*3+2] = in[i*4+0] * matrix[2][0] + in[i*4+1] * matrix[2][1] + in[i*4+2] * matrix[2][2] + in[i*4+3] * matrix[2][3];
   }
   int64_t to_time = time_in_micros();
 
